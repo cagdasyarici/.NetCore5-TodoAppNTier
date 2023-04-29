c39a879d18d8efb4afc1f112046d6d1499c73572
cagdasyarici <cagdasyarici16@gmail.com>
2023-04-29 22:52:23 +0300

    bla

--- Udemy.TodoAppNTier.Business/Interfaces/IWorkService.cs
+++ Udemy.TodoAppNTier.Business/Interfaces/IWorkService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Udemy.TodoAppNTierDtos.WorkDtos;
+using Udemy.TodoAppNTier.Dtos.WorkDtos;
 
 namespace Udemy.TodoAppNTier.Business.Interfaces
 {
